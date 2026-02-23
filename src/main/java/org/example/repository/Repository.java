d06956bc7b57e7a0ec4959a96a3b4ebdc646bba2
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2026-02-23 20:20:33 +0700

    3.1 Interface Repository

---
+++ src/main/java/org/example/repository/Repository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import java.util.*;
+
+public interface Repository<T> {
+    void add(T item);
+    boolean remove(T item);
+    Optional<T> findById(String id);
+    List<T> findAll();
+    int count();
+    void clear();
+}
